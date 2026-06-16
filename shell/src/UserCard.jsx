ebd76bf2b434e2d58fe2f2f184b2a552d5595613
rohit gudse <rohitgudse@gmail.com>
2026-06-16 18:00:02 +0530

    adding three components on this update

---
+++ shell/src/UserCard.jsx
@@ -0,0 +1,8 @@
+export default function UserCard(){
+    return(
+        <div>
+            <h3>User Card</h3>
+            <p>Rohit Gudse</p>
+        </div>
+    );
+}
---
+++ shell/src/Welcome.jsx
@@ -0,0 +1,5 @@
+export default function Welcome() {
+    return 
+        <h2>Welcome to <Mark></Mark>icro Frontend App</h2>;
+    
+}
