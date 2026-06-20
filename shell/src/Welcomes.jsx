465da48d78e13ea8ea8c287717a1220019771fb8
rohit gudse <rohitgudse@gmail.com>
2026-06-20 18:18:19 +0530

    adding more file

--- shell/src/Welcomes.jsx
+++ shell/src/Welcomes.jsx
@@ -10,4 +10,16 @@ function Welcome() {
     );
 }
 
+function Welcome() {
+    return (
+        <div>
+            <h2>
+                Welcome to Mico frontend app 
+            </h2>
+        </div>
+    );
+}
+
+
+
 export default Welcome; 
